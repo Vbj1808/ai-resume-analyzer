043f2dfee2ba0fb5325091619bce9cb92e4c6d9b
Vbj1808 <ijalab1808@gmail.com>
2025-07-21 23:20:45 -0400

    implemented resume feedback

--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -59,6 +59,7 @@ const upload = () => {
 
         setStatusText('Analysis complete, redirecting...');
         console.log(data);
+        navigate(`/resume/${uuid}`);
     }
 
     const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
